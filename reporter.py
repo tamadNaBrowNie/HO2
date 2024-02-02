12164b5757af1bb514a9ae1a7d100e0308be1098
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-02-02 19:08:57 +0800

    fixed errors

--- reporter.py
+++ reporter.py
@@ -5,11 +5,11 @@ import mysql.connector as sql
 
 args = []
 param = ['user','password','host','port','database']
-for prompt in prompts:
-	params.append(input(prompt))
+for prompt in param:
+	args.append(input(prompt))
 config =dict(zip(param,args))
 try:
-	db = sql.connect(**params)
+	db = sql.connect(**config)
 	cursor = db.cursor()
 
 except sql.Error as err:
